f9483eb89d84b300778bc6a3cb0f75aef3082c03
Dylan Hiemstra <dylan@noardcode.nl>
2020-03-28 20:44:22 +0100

    Subscribe and challenge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,29 +8,29 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            StrategicGameClient.getInstance().getState().connect("localhost", 7789);
-            StrategicGameClient.getInstance().getState().login("Dylan");
-            JSONArray gameList = StrategicGameClient.getInstance().getGameList();
+//        try {
+//            StrategicGameClient.getInstance().getState().connect("localhost", 7789);
+//            StrategicGameClient.getInstance().getState().login("Dylan");
+//            JSONArray gameList = StrategicGameClient.getInstance().getGameList();
+//
+//            for(Object game : gameList) {
+//                System.out.println((String) game);
+//            }
+//
+//            JSONArray playerList = StrategicGameClient.getInstance().getPlayerList();
+//
+//            for(Object player : playerList) {
+//                System.out.println((String) player);
+//            }
+//
+//            StrategicGameClient.getInstance().getState().startWaiting();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Application.launch(DebugBar.class);
 
-            for(Object game : gameList) {
-                System.out.println((String) game);
-            }
-
-            JSONArray playerList = StrategicGameClient.getInstance().getPlayerList();
-
-            for(Object player : playerList) {
-                System.out.println((String) player);
-            }
-
-            StrategicGameClient.getInstance().getState().startWaiting();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Application.launch(DebugBar.class);
-
-        //Application.launch(BoardSetup.class);
+        Application.launch(BoardSetup.class);
     }
 }
